1fd286c1e66620b284be0b82baa1e49b2a5451b7
thomsoke <33559383+thomsoke@users.noreply.github.com>
2020-02-18 19:11:06 -0800

    header

--- src/alert-box/alert-box.js
+++ src/alert-box/alert-box.js
@@ -1,19 +1,23 @@
 // Says Hello to the user when clicked!
 
 /**
- * This format uses the ES2015/ES6 class. 
- * 
- * You define an export before 'class' and you then import 
+ * This format uses the ES2015/ES6 class.
+ *
+ * You define an export before 'class' and you then import
  * it by referencing the path of this file and then referring it
  * with curly braces:
- * 
+ *
  * import { AlertBox } from './alert-box/alert-box;
- * 
+ *
  * Note that AlertBox is the same name as your class!
- * 
+ *
  */
-export class AlertBox {
+class AlertBox {
     sayHi() {
-        alert("Hello 😊");
+        alert("Hello 😊big kel");
     }
 }
+
+export const NAME = "Franklin"
+
+export { AlertBox }
---
+++ src/header/header.js
@@ -0,0 +1,8 @@
+const generateHeader = (htmlElement) => {
+  const mainHeader = document.createElement("h1")
+  mainHeader.id = "main-header"
+  mainHeader.innerHTML = "Ultimate Grand Supreme Show Tracker Experience 1.0"
+  htmlElement.appendChild(mainHeader)
+}
+
+export { generateHeader }
