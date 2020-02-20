3d92e660b37b9fcd3172baa9768428f206207c0b
thomsoke <33559383+thomsoke@users.noreply.github.com>
2020-02-19 17:17:05 -0800

    add constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -33,17 +33,19 @@ export { CONSTANTS }
 
 
 
-
-object called CONSTANTS with two sub-objects subtitle and description
-
-
-
-four objects (home, subpage1, subpage2, subpage3) with three
-data members each (childtext, classname, href)
-
-we put those objects into an array called METADATA_ITEMS
-
-in generateSubheader we call fillSubheaderArray and pass it METADATA_ITEMS
-fillSubheaderArray fills an array with the contents of the METADATA_ITEMS
-we then use a loop to append each index of the produced array to the
-html element (our div in index.html)
+// *** what's happeneing with the stuff at the top of subheader-items.js...
+// *** becuz that's kinda what's happening here
+//
+// object called CONSTANTS with two sub-objects subtitle and description
+//
+//
+//
+// four objects (home, subpage1, subpage2, subpage3) with three
+// data members each (childtext, classname, href)
+//
+// we put those objects into an array called METADATA_ITEMS
+//
+// in generateSubheader we call fillSubheaderArray and pass it METADATA_ITEMS
+// fillSubheaderArray fills an array with the contents of the METADATA_ITEMS
+// we then use a loop to append each index of the produced array to the
+// html element (our div in index.html)
